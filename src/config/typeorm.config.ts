98e642fa4f3074c0904359b35172394c84c1f263
Hubert Józwowiak <hubert.jozwowiak@appunite.com>
2021-06-23 01:26:03 +0200

    wip

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DATABASE_URL || '127.0.0.1',
+  host: process.env.DATABASE_HOST || '127.0.0.1',
   port: Number(process.env.DATABASE_PORT) || 3306,
   username: process.env.DATABASE_USER || 'root',
   password: process.env.DATABASE_PASSWORD || '',
